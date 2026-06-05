016f5b0a379babcf31cd8c13398ee2eb4b6a5dc6
Dagudelo-WSL-D <agudelo.david@hotmail.com>
2026-06-05 12:15:20 -0400

    19

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nombre")
+    description = models.TextField(max_length=300, verbose_name="Descripción")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    image = models.ImageField(upload_to='products/images/', verbose_name="Imagen")
+    photo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name="Foto")
+    available = models.BooleanField(default=True, verbose_name="Disponible")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
+
+    def __str__(self):
+        return self.name
