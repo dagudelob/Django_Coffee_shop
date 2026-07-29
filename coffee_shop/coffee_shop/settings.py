fbc3dc3feff70af23386bd982a96e598c0971449
Dagudelo-WSL-D <agudelo.david@hotmail.com>
2026-07-29 12:26:40 -0400

    security: update gitignore for secrets and media 2

--- coffee_shop/coffee_shop/settings.py
+++ coffee_shop/coffee_shop/settings.py
@@ -75,13 +75,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "coffee_shop.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "coffee_shop"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
